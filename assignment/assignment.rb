359e40e94903c71e4950702c3d8ed12970338e63
swaption2009 <swaption2009@gmail.com>
2015-12-12 23:31:17 -0800

    complete rspec test 5-8

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -33,61 +33,43 @@ class Assignment
   # Query DB with exact match
   #
   def find_user_byname(username)
-      # accept a username input parameter
-      # use the User Model class to find all Users with the supplied username.
-      # NOTE:  Username is not unique in the Users table, thus you can have many users with the same username.
-      # return a collection of User instances that match the provided username
+    @user = User.where(username: username)
   end
 
   def find_todolist_byname(name)
-      # accept a name input parameter
-      # use the TodoList Model class to find all TodoLists with the supplied list_name.
-      # NOTE: List name is not required to be unique, thus you can have many lists with the same list name.
-      # return a collection of TodoList instances that match the provided name
+    @todolist = TodoList.where(list_name: name)
   end
 
   #
   # Get rows from DB by PK
   #
   def get_user_byid(id)
-      # accept an id input parameter
-      # use the User Model class to get the User associated with the `id` primary key
-      # return the User instance that matches the provided id
+    @user = User.find_by_id(id)
   end
 
   def get_todolist_byid(id)
-      # accept an id input parameter
-      # use the TodoList Model class to get the TodoList associated with the `id` primary key
-      # return the TodoList instance that matches the provided id
+    @todolist = TodoList.find_by_id(id)
   end
 
   #
   # Update rows in DB
   #
   def update_password(id, password_digest)
-      # accept an id and password_digest input parameters
-      # use the User Model class to update the `password_digest` for the User associated with the id primary key
-      # (no return is required)
+    @user = User.update(id, password_digest: password_digest)
   end
 
   def update_listname(id, name)
-      # accept an id and name input parameters
-      # use the TodoList Model class to update the `list_name` for the TodoList associated with id primary key
-      # (no return is required)
+    @todolist = TodoList.update(id, list_name: name)
   end
 
   #
   # Delete rows from DB
   #
   def delete_user(id)
-      # accept an id input parameter
-      # use the User Model class to remove the User associated with the `id` primary key from the database
-      # (no return is required)
+    @user = User.destroy(id)
   end
 
   def delete_todolist(id)
-      # accept an id input parameter
-      # use the TodoList Model class to remove the TodoList associated with the `id` primary key.
-      # (no return is required)
+    @todolist = TodoList.destroy(id)
   end
 end
