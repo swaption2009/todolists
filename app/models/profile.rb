34f8ee4f3d56c24756c3d6f4412160c385e7fed4
swaption2009 <swaption2009@gmail.com>
2015-12-16 01:49:53 -0800

    Complete P2

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,21 @@
 class Profile < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
+
+  validates_inclusion_of :gender, :in => %w(male female)
+  validate :permit_either_first_or_last_name_to_be_null,
+           :cannot_be_male_if_name_is_sue
+
+  def permit_either_first_or_last_name_to_be_null
+    errors.add(:first_name, "name can't be null") if
+      first_name.nil? && last_name.nil?
+  end
+
+  def cannot_be_male_if_name_is_sue
+    errors.add(:gender, "cannot be male if name is Sue") if
+      first_name == 'Sue' && gender == 'male'
+  end
+
+  def self.get_all_profiles(startYear, endYear)
+    where("birth_year BETWEEN #{startYear} AND #{endYear}").order(birth_year: :asc)
+  end
 end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,6 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   belongs_to :user
+
+  default_scope { order('due_date') }
 end
