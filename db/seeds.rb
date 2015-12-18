7f14ef8dcd5b12e976ed853270983357888a0d3c
swaption2009 <swaption2009@gmail.com>
2015-12-18 14:36:14 -0800

    pass start_spec.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,27 @@
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
+
 User.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
-Profile.destroy_all
 
-Profile.create! [
-  { first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female" },
-  { first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male" },
-  { first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male" },
-  { first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female" },
-]
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-Profile.find_by!(last_name: "Fiorina").create_user(username: "Fiorina", password_digest: "123456")
-Profile.find_by!(last_name: "Trump").create_user(username: "Trump", password_digest: "123456")
-Profile.find_by!(last_name: "Carson").create_user(username: "Carson", password_digest: "123456")
-Profile.find_by!(last_name: "Clinton").create_user(username: "Clinton", password_digest: "123456")
-
-User.all.each do |user|
-  todo_list = TodoList.create!(list_name: "User TodoList", list_due_date: Time.now + 1.year.to_i)
-  user.todo_lists << todo_list
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
 end
 
-TodoList.all.each do |todo_list|
-  5.times do
-    todo_item = TodoItem.create!(title: "Todo Item", description: "Part of TodoList", due_date: Time.now + 1.year.to_i)
-    todo_list.todo_items << todo_item
-  end
+users = User.create! [
+  { username: "jim", password_digest: "abc123" },
+  { username: "rich", password_digest: "123abc" }
+]
+
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
